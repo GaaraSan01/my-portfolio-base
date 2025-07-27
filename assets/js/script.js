960833e9f98c335266562e91ec97f180bebaf994
Vinícius H. <91447833+GaaraSan01@users.noreply.github.com>
2025-07-27 00:42:56 -0300

    Init configuration acordeon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,13 @@
 (function (){
-    
+
+    // Menu
     const btnMenu = document.querySelector(".mobile-menu");
     const menu = document.querySelector(".nav-content-mobile");
 
     btnMenu.addEventListener('click', () => {
         menu.classList.toggle("ativa");
     });
+
+    // Acordeon
+    
 })()
